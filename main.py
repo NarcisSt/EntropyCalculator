85aa20a8972ab7c0d8a3618344e29cb5574f616a
NarcisSt <75793612+NarcisSt@users.noreply.github.com>
2021-10-19 21:57:07 +0300

    infinite loop

--- main.py
+++ main.py
@@ -34,28 +34,30 @@ def conditionalEntropy(root, first_partition, second_partition):
 
 
 if __name__ == '__main__':
-    print("\nIntroduceti prima partitie descendenta: ")
-    a1 = int(input("+: "))
-    b1 = int(input("-: "))
-    partition1 = [a1, b1]
+    while True:
+        print("\nIntroduceti prima partitie descendenta: ")
+        a1 = int(input("+: "))
+        b1 = int(input("-: "))
+        partition1 = [a1, b1]
 
-    print("\nIntroduceti a doua partitie descendenta: ")
-    a2 = int(input("+: "))
-    b2 = int(input("-: "))
-    partition2 = [a2, b2]
+        print("\nIntroduceti a doua partitie descendenta: ")
+        a2 = int(input("+: "))
+        b2 = int(input("-: "))
+        partition2 = [a2, b2]
 
-    r1 = a1 + a2
-    r2 = b1 + b2
-    root = [r1, r2]
+        r1 = a1 + a2
+        r2 = b1 + b2
+        root = [r1, r2]
 
-    print("\nCompasul de decizie rezultat este:")
-    print("\n           [" + str(r1) + "+, " + str(r2) + "-]            ")
-    print("\n   [" + str(a1) + "+, " + str(b1) + "-]" + "        " + "[" + str(a2) + "+, " + str(b2) + "-]")
+        print("\nCompasul de decizie rezultat este:")
+        print("\n           [" + str(r1) + "+, " + str(r2) + "-]            ")
+        print("\n   [" + str(a1) + "+, " + str(b1) + "-]" + "        " + "[" + str(a2) + "+, " + str(b2) + "-]")
 
-    print("\nEntropia atributului de iesire este : " + str(simpleEntropy(root)))
-    print("\nEntropia specifica primei partitii este : " + str(simpleEntropy(partition1)))
-    print("\nEntropia specifica celei de-a doua partitii este : " + str(simpleEntropy(partition2)))
-    print("\nEntropia conditionala a atributului de iesire este : " + str(
-        conditionalEntropy(root, partition1, partition2)))
-    print("\nCastigul de informatie al atributului de iesire este : " + str(
-        simpleEntropy(root) - conditionalEntropy(root, partition1, partition2)))
+        print("\nEntropia atributului de iesire este : " + str(simpleEntropy(root)))
+        print("\nEntropia specifica primei partitii este : " + str(simpleEntropy(partition1)))
+        print("\nEntropia specifica celei de-a doua partitii este : " + str(simpleEntropy(partition2)))
+        print("\nEntropia conditionala a atributului de iesire este : " + str(
+            conditionalEntropy(root, partition1, partition2)))
+        print("\nCastigul de informatie al atributului de iesire este : " + str(
+            simpleEntropy(root) - conditionalEntropy(root, partition1, partition2)))
+        print("\n")
