00a181d9b46ba62e2a9e07a7339485cc6205b349
NarcisSt <75793612+NarcisSt@users.noreply.github.com>
2021-10-19 21:08:39 +0300

    Added calculator

---
+++ main.py
@@ -0,0 +1,61 @@
+import math
+
+
+def getSumOfPartition(partition):
+    a = partition[0]
+    b = partition[1]
+    s = a + b
+
+    return s
+
+
+def simpleEntropy(partition):
+    a = partition[0]
+    b = partition[1]
+    s = getSumOfPartition(partition)
+
+    if a == 0 or b == 0:
+        entropy = 0
+    else:
+        entropy = a / s * math.log2(s / a) + b / s * math.log2(s / b)
+
+    return entropy
+
+
+def conditionalEntropy(root, first_partition, second_partition):
+    s_part1 = getSumOfPartition(first_partition)
+    s_part2 = getSumOfPartition(second_partition)
+    s_root = getSumOfPartition(root)
+
+    conditional_entropy = s_part1 / s_root * simpleEntropy(first_partition) + s_part2 / s_root * simpleEntropy(
+        second_partition)
+
+    return conditional_entropy
+
+
+if __name__ == '__main__':
+    print("\nIntroduceti prima partitie descendenta: ")
+    a1 = int(input("+: "))
+    b1 = int(input("-: "))
+    partition1 = [a1, b1]
+
+    print("\nIntroduceti a doua partitie descendenta: ")
+    a2 = int(input("+: "))
+    b2 = int(input("-: "))
+    partition2 = [a2, b2]
+
+    r1 = a1 + a2
+    r2 = b1 + b2
+    root = [r1, r2]
+
+    print("\nCompasul de decizie rezultat este:")
+    print("\n           [" + str(r1) + "+, " + str(r2) + "-]            ")
+    print("\n   [" + str(a1) + "+, " + str(b1) + "-]" + "        " + "[" + str(a2) + "+, " + str(b2) + "-]")
+
+    print("\nEntropia atributului de iesire este : " + str(simpleEntropy(root)))
+    print("\nEntropia specifica primei partitii este : " + str(simpleEntropy(partition1)))
+    print("\nEntropia specifica celei de-a doua partitii este : " + str(simpleEntropy(partition2)))
+    print("\nEntropia conditionala a atributului de iesire este : " + str(
+        conditionalEntropy(root, partition1, partition2)))
+    print("\nCastigul de informatie al atributului de iesire este : " + str(
+        simpleEntropy(root) - conditionalEntropy(root, partition1, partition2)))
---
+++ readme.md
@@ -0,0 +1,31 @@
+## Entropy calculator
+Author: Barat Narcis-Stefan
+
+###Output
+```bash
+Introduceti prima partitie descendenta: 
++: 5
+-: 1
+
+Introduceti a doua partitie descendenta: 
++: 0
+-: 3
+
+Compasul de decizie rezultat este:
+
+           [5+, 4-]            
+
+   [5+, 1-]        [0+, 3-]
+
+Entropia atributului de iesire este : 0.9910760598382222
+
+Entropia specifica primei partitii este : 0.6500224216483541
+
+Entropia specifica celei de-a doua partitii este : 0
+
+Entropia conditionala a atributului de iesire este : 0.4333482810989027
+
+Castigul de informatie al atributului de iesire este : 0.5577277787393196
+
+Process finished with exit code 0
+```
